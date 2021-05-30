53800b8904db63b89ebf1a89269cacd47b610260
Federico Gentile <fgentile95dev@icloud.com>
2021-05-31 00:20:14 +0200

    Update SearchTextField.swift

--- SearchTextField/Classes/SearchTextField.swift
+++ SearchTextField/Classes/SearchTextField.swift
@@ -575,7 +575,7 @@ extension SearchTextField: UITableViewDelegate, UITableViewDataSource {
         cell!.layoutMargins = UIEdgeInsets.zero
         cell!.preservesSuperviewLayoutMargins = false
         cell!.textLabel?.font = theme.font
-        cell!.detailTextLabel?.font = UIFont(name: theme.font.fontName, size: theme.font.pointSize * fontConversionRate)
+        cell!.detailTextLabel?.font = theme.font.withSize(theme.font.pointSize * fontConversionRate)
         cell!.textLabel?.textColor = theme.fontColor
         cell!.detailTextLabel?.textColor = theme.subtitleFontColor
         
